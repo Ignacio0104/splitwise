4c320fd53862ff72c477857c5e39b889f4c62a37
Ignacio Martin Smirlian <ignacio.smirlian@hotmail.com>
2025-09-06 00:35:30 -0300

    dropdown working ok

--- app/reportForms/creationForm.tsx
+++ app/reportForms/creationForm.tsx
@@ -1,15 +1,17 @@
 import { Theme } from '@/constants/Colors';
-import { EFonts } from '@/constants/styleUtils';
+import { center, EFonts } from '@/constants/styleUtils';
 import { BASE_WIDTH } from '@/constants/Values';
 import { router } from 'expo-router';
 import { Formik } from 'formik';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, useWindowDimensions, View } from 'react-native';
-import { Appbar } from 'react-native-paper';
+import { Appbar, Switch } from 'react-native-paper';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { reportFormSchema, reportInitialValues } from './schemas/report-form-schema';
 import { LoginModel } from '../login/models/loginModel';
 import { ReportFormModel } from './models/reportFormsModels';
+import RNPickerSelect from 'react-native-picker-select';
+import { reportDropdownOption } from '../store/models';
 
 export function useStyles(fontSize?: number) {
   const { width } = useWindowDimensions();
@@ -45,10 +47,6 @@ export function useStyles(fontSize?: number) {
       display: 'flex',
       gap: 10,
     },
-    expenseNameLabel: {
-      color: Theme.grayFont,
-      fontFamily: EFonts.MONTSERRAT_REGULAR,
-    },
     expenseTextInput: {
       backgroundColor: Theme.grayBackground,
       borderRadius: 10,
@@ -56,11 +54,63 @@ export function useStyles(fontSize?: number) {
       color: Theme.whiteFont,
       paddingLeft: 15,
     },
+    dropdownContainer: {
+      marginTop: 20,
+      display: 'flex',
+      gap: 10,
+    },
+    formLabel: {
+      color: Theme.grayFont,
+      fontFamily: EFonts.MONTSERRAT_REGULAR,
+    },
+    dropdown: {
+      backgroundColor: Theme.grayBackground,
+      borderRadius: 10,
+      height: 60,
+    },
+    fixedPriceContainer: {
+      marginTop: 30,
+      ...center,
+      justifyContent: 'space-around',
+    },
+    fixedPriceText: {
+      fontSize: aspectRatio * 18,
+    },
+    totalNumberContainer: {
+      marginTop: 20,
+      display: 'flex',
+      gap: 10,
+    },
   });
 }
 
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    backgroundColor: Theme.grayBackground,
+    borderRadius: 20,
+    height: 60,
+    color: Theme.whiteFont,
+  },
+  inputAndroid: {
+    backgroundColor: Theme.grayBackground,
+    borderRadius: 20,
+    height: 60,
+    color: Theme.whiteFont,
+  },
+  viewContainer: {
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  switchStyle: {
+    fontSize: 40,
+  },
+});
+
 export default function CreationForm() {
   const styles = useStyles();
+
+  const [selectedType, setSelectedType] = useState<string>(reportDropdownOption[0].value);
+  const [fixedPrice, setFixedPrice] = useState<boolean>(false);
 
   const handleSubmit = (values: ReportFormModel) => {
     console.log(values);
@@ -91,9 +141,36 @@ export default function CreationForm() {
           {({ handleChange, handleBlur, handleSubmit, values, errors, touched, submitCount }) => (
             <View style={styles.formContainer}>
               <View style={styles.expenseNameInputContainer}>
-                <Text style={styles.expenseNameLabel}>Nombre Reporte</Text>
+                <Text style={styles.formLabel}>Nombre Reporte</Text>
                 <TextInput
                   placeholder="eg,. Cena con amigos"
+                  placeholderTextColor={Theme.grayFont}
+                  style={styles.expenseTextInput}
+                />
+              </View>
+              <View style={styles.dropdownContainer}>
+                <Text style={styles.formLabel}>Tipo</Text>
+                <RNPickerSelect
+                  onValueChange={(value) => setSelectedType(value)}
+                  items={reportDropdownOption}
+                  placeholder={{ label: 'Selecciona un tipo...', value: null }}
+                  value={selectedType}
+                  style={pickerSelectStyles}
+                />
+              </View>
+              <View style={styles.fixedPriceContainer}>
+                <Text style={[styles.formLabel, styles.fixedPriceText]}> Precio fijo </Text>
+                <Switch
+                  color={Theme.greenHiglight}
+                  value={fixedPrice}
+                  onValueChange={() => setFixedPrice(!fixedPrice)}
+                />
+              </View>
+              <View style={(styles.totalNumberContainer, { display: fixedPrice ? 'flex' : 'none' })}>
+                <Text style={styles.formLabel}>Total</Text>
+                <TextInput
+                  placeholder="0"
+                  keyboardType="numeric"
                   placeholderTextColor={Theme.grayFont}
                   style={styles.expenseTextInput}
                 />
--- app/store/models.ts
+++ app/store/models.ts
@@ -1,7 +1,15 @@
 export enum ReportType {
-  DINNER = "dinner",
-  HOUSEHOLD = "household",
-  TRIP = "trip",
-  PARTY = "party",
-  SHOPPING = "shopping",
+  DINNER = 'dinner',
+  HOUSEHOLD = 'household',
+  TRIP = 'trip',
+  PARTY = 'party',
+  SHOPPING = 'shopping',
 }
+
+export const reportDropdownOption = [
+  { label: 'Cena', value: ReportType.DINNER },
+  { label: 'Hogar', value: ReportType.HOUSEHOLD },
+  { label: 'Viaje', value: ReportType.TRIP },
+  { label: 'Fiesta', value: ReportType.PARTY },
+  { label: 'Shopping', value: ReportType.SHOPPING },
+];
